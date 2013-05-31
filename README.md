eda8cacd266ae4769ad01c91b47833f4aa603049
Yuchen Xie <yuchen.xie@live.com>
2013-05-31 14:45:01 +0800

    add some macros

--- README.md
+++ README.md
@@ -1,4 +1,48 @@
 mongodb-utils
 =============
 
-Mongodb utilities for debuging.
+Mongodb utilities.
+
+<table>
+  <tr><th colspan="2" style="text-align:center;">Improved BSONObj builder macros</th></tr>
+  <tr>
+    <td><code>BSONS</code></td>
+    <td><pre>Use BSONS macro to build a BSONObj of custom initial buffer size from a stream to reduce memory allocation times.
+
+    e.g.,
+       <code>BSONS(32, "name" << "joe" << "age" << 33 )</code>
+
+    with auto-generated object id:
+       <code>BSONS(64, GENOID << "name" << "joe" << "age" << 33 )</code>
+    when size is 64, it is the same as:
+       <code>BSON(GENOID << "name" << "joe" << "age" << 33 )</code>
+
+    The labels GT, GTE, LT, LTE, NE can be helpful for stream-oriented construction
+    of a BSONObj, particularly when assembling a Query.  For example,
+    <code>BSONS(96, "a" << GT << 23.4 << NE << 30 << "b" << 2 )</code> produces the object
+    <code>{ a: { $gt: 23.4, $ne: 30 }, b: 2 }.</code>
+</pre>
+</td>
+  </tr>
+  <tr>
+    <td><code>BSONS_ARRAY</code></td>
+    <td><pre>Use BSONS_ARRAY macro like BSONS macro, but without keys
+    <code>BSONArray arr = BSONS_ARRAY(128, "hello" << 1 <<
+        BSON("foo" << BSONS_ARRAY(32, "bar" << "baz" << "qux" ) ) );</code></pre>
+  </td>
+  </tr>
+  <tr><th colspan="2" style="text-align:center;">Debug helpers</th></tr>
+  <tr>
+    <td><code>SHOW_BSON_BOB</code></td>
+    <td>Print bson::bob object content only in DEBUG mode.</td>
+  </tr>
+  <tr>
+    <td><code>SHOW_BSON_BO</code></td>
+    <td>Print bson::bo object content only in DEBUG mode.</td>
+  </tr>
+  <tr>
+    <td><code>SHOW_BSON_BE</code></td>
+    <td>Print bson::be object content only in DEBUG mode.</td>
+  </tr>
+</table>
+
---
+++ macros.h
@@ -0,0 +1,80 @@
+#ifndef MAPX_MONOGODB_MACROS_H
+#define MAPX_MONOGODB_MACROS_H
+
+#include <cassert>
+#ifndef NDEBUG
+#include <cstdio>
+#endif
+
+// where BSON and BOSN_ARRAY is defined
+#include <bson/bsonmisc.h>
+
+
+/** Use BSONS macro to build a BSONObj of custom initial buffer size from a stream
+    to reduce memory allocation times.
+
+    e.g.,
+       BSONS(32, "name" << "joe" << "age" << 33 )
+
+    with auto-generated object id:
+       BSONS(64, GENOID << "name" << "joe" << "age" << 33 )
+    when size is 64, it is the same as:
+       BSON(GENOID << "name" << "joe" << "age" << 33 )
+
+    The labels GT, GTE, LT, LTE, NE can be helpful for stream-oriented construction
+    of a BSONObj, particularly when assembling a Query.  For example,
+    BSONS(96, "a" << GT << 23.4 << NE << 30 << "b" << 2 ) produces the object
+    { a: { \$gt: 23.4, \$ne: 30 }, b: 2 }.
+*/
+#define BSONS(size, x) (( mongo::BSONObjBuilder(size) << x ).obj())
+
+
+/** Use BSONS_ARRAY macro like BSONS macro, but without keys
+
+    BSONArray arr = BSONS_ARRAY(128, "hello" << 1 <<
+        BSON("foo" << BSONS_ARRAY(32, "bar" << "baz" << "qux" ) ) );
+
+ */
+#define BSONS_ARRAY(size, x) (( mongo::BSONArrayBuilder(size) << x ).arr())
+
+
+/**
+ * Print bson::bob of the format:
+ * __FILE__:__LINE__ bob[<buffer_size>]=<bob content>
+ * Only available in DEBUG mode.
+ * WARNING: expression b is calculated twice, which might have
+ * side effects.
+ */
+#define SHOW_BSON_BOB(b) assert(printf("%s:%d bob[%d]=%s\n", \
+      __FILE__, __LINE__, \
+      (b).asTempObj().objsize(), \
+      (b).asTempObj().jsonString().c_str()))
+
+
+/**
+ * Print bson::bo of the format:
+ * __FILE__:__LINE__ bo[<buffer_size>]=<bo content>
+ * Only available in DEBUG mode.
+ * WARNING: expression b is calculated twice, which might have
+ * side effects.
+ */
+#define SHOW_BSON_BO(b) assert(printf("%s:%d bo[%d]=%s\n", \
+      __FILE__, __LINE__, \
+      (b).objsize(), \
+      (b).jsonString().c_str()))
+
+
+/**
+ * Print bson::be of the format:
+ * __FILE__:__LINE__ be[<buffer_size>]=<be content>
+ * Only available in DEBUG mode.
+ * WARNING: expression b is calculated twice, which might have
+ * side effects.
+ */
+#define SHOW_BSON_BE(b) assert(printf("%s:%d be[%d]=%s\n", \
+      __FILE__, __LINE__, \
+      (b).objsize(), \
+      (b).toString().c_str()))
+
+#endif
+
